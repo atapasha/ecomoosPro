7594ada568e7f9972895b189c93fbb2c26570acc
atapasha <atapashazade@gmail.com>
2025-09-12 17:17:30 +0330

    errors hanleling added

--- server/controllers/user/usersingle.js
+++ server/controllers/user/usersingle.js
@@ -1,16 +1,18 @@
+const Usertable = require("../../Models/usertable");
 
-
-const usersingle=(req,res)=>{
-
-    try{
-        res.send("this is usersingle",req);
-    }catch(errors){
-        res.send("this is  error",req);
-
-
+const usersingle = async (req, res) => {
+  try {
+    const userId = req.params.id;
+    const userdetail = await Usertable.findById(userId);
+    if (!userdetail) {
+      return res.status(404).send({ errors: "user detail not found" });
+    } else {
+      res.status(200).send({ status: "successfully", data: userdetail });
     }
 
+    res.send("this is usersingle", req.params.id);
+  } catch (errors) {
+res.status(500).send({errors:"An error happen while fetching",servererror:errors})  }
+};
 
-}
-
-module.exports=usersingle
+module.exports = usersingle;
